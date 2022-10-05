0bff75c97050ac4a2eb5159f191c793083aaa3d7
Nico <nicostringa@outlook.com>
2022-10-05 20:46:21 -0300

    añadidos assets, root y font en el css, array temporal en js

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Bennazianna",
+      precio: 3650,
+      info: "La más completa",
+      imagen: '../assets/Bennazianna.png'
+    },
+    {
+      id: 2,
+      nombre: "Tronco-Pizza",
+      precio: 870,
+      info: "Para todo el día",
+      imagen: '../assets/TroncoPizza.png'
+    },
+    {
+      id: 3,
+      nombre: "Papas | Provenzal",
+      precio: 360,
+      info: "Van como piña",
+      imagen: '../assets/PapasProvenzal.png'
+    },
+    {
+      id: 4,
+      nombre: "La Mr. Pit",
+      precio: 350,
+      info: "Solo para expertos",
+      imagen: '../assets/MrPit.png'
+    },
+    {
+      id: 5,
+      nombre: "Q'Jamone",
+      precio: 350,
+      info: "c/jamón crudo",
+      imagen: '../assets/QJamone.png'
+    },
+    {
+      id: 6,
+      nombre: "La Charly García",
+      precio: 380,
+      info: "¡BASTA!",
+      imagen: '../assets/CharlyGarcia.png'
+    },
+    {
+      id: 7,
+      nombre: "La Maradona",
+      precio: 450,
+      info: "¡Eterna!",
+      imagen: '../assets/Maradona.png'
+    },
+    {
+      id: 8,
+      nombre: "Picantovich",
+      precio: 750,
+      info: "Pica 2 veces",
+      imagen: '../assets/Picantovich.png'
+    },
+    {
+      id: 9,
+      nombre: "La Hasbulla",
+      precio: 990,
+      info: "En honor al 1",
+      imagen: '../assets/Hasbulla.png'
+    },
+    {
+      id: 10,
+      nombre: "Leo Messi",
+      precio: 10,
+      info: "¡De pié señores!",
+      imagen: '../assets/LeoMessi.png'
+    },
+    {
+      id: 11,
+      nombre: "Nick Gi",
+      precio: 0,
+      info: "La que desaparece",
+      imagen: '../assets/NickGi.png'
+    },
+];
